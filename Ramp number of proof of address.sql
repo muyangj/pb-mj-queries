d3dfc0df47264775f68dce11723b61264e441c5d
muyang <muyang@passbsase.com>
2022-07-04 17:15:08 +0200

    k

--- Ramp number of proof of address.sql
+++ Ramp number of proof of address.sql
@@ -1,8 +1,9 @@
 SELECT
 	COUNT((identity_accesses)) AS "NumberofProof_OF_ADDRESS",
-	organizations.id AS "Organizations_id",
-	organizations.name AS "Organizations_name",
-	resources.type as "Resources_type"
+	organizations.id AS "Organization_id",
+	organizations.name AS "Organization_name",
+	projects.id AS "Project_id",
+	resources.type AS "Resource_type"
 FROM
 	resources
 	LEFT JOIN identities ON resources.identity_id = identities.id
@@ -10,11 +11,14 @@ FROM
 	LEFT JOIN projects ON identity_accesses.project_id = projects.id
 	LEFT JOIN organizations ON projects.organization_id = organizations.id
 WHERE
-	TO_CHAR (identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-05-07'
-	AND TO_CHAR (identity_accesses.created_at, 'YYYY-MM-DD') < '2022-06-07'
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-05-07'
+	AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-06-07'
 	AND resources.type = 'PROOF_OF_ADDRESS'
 	AND organizations.id = '972' -- ramp org id
 GROUP BY
 	organizations.id,
 	organizations.name,
+	projects.id,
 	resources.type
+ORDER BY
+	"NumberofProof_OF_ADDRESS" DESC;
