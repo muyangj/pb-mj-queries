363e67bc042050af359a16bcddaaf67ab39cdf56
muyang <muyang@passbsase.com>
2022-07-04 11:20:57 +0200

    changed file name

---
+++ Numberofverifications by project_id & org_id & date.sql
@@ -0,0 +1,18 @@
+SELECT
+	COUNT((identity_accesses.id)) AS "NumberOfVerifications",
+	identity_accesses.project_id as "project_id",
+	projects.organization_id as "organization_id",
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') AS "identity_accesses_created_at"
+FROM
+	identity_accesses
+	LEFT JOIN projects ON identity_accesses.project_id = projects.id
+	LEFT JOIN organizations ON projects.organization_id = organizations.id
+WHERE
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-06-01'
+	AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-07-01'
+GROUP BY
+	identity_accesses.project_id,
+	projects.organization_id,
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD')
+ORDER BY
+	"NumberOfVerifications" DESC;
