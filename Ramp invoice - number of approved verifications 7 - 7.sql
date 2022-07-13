d37ceeb9f0e59bbe6cafcf11c8aa32e78b5b7431
muyang <muyang@passbsase.com>
2022-07-13 14:42:05 +0200

    added number of ongoing monitoring count

--- Ramp invoice - number of approved verifications 7 - 7.sql
+++ Ramp invoice - number of approved verifications 7 - 7.sql
@@ -3,7 +3,8 @@ SELECT
 	organizations.id AS "Organization_id",
 	organizations.name AS "Organization_name",
 	organizations.stripe_customer_id AS "Stripe_id",
-	projects.id AS "Project_id",
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM') AS "IA_created_at",
+	--projects.id AS "Project_id",
 	identity_accesses.status as "Status"
 FROM
     identity_accesses
@@ -11,17 +12,17 @@ FROM
     LEFT JOIN projects ON identity_accesses.project_id = projects.id
     LEFT JOIN organizations on projects.organization_id = organizations.id
 WHERE
-    -- IDs are for the following Slugs same order ramp-0a5fd2a7, ramp-e8c15113,
-    organizations.id = '972'
-    AND identity_accesses.project_id IN (1962, 6783) 
-    AND identity_accesses.status IN (6)
+    organizations.id = '972' -- Ramp org_id is 972
+    AND identity_accesses.project_id IN (1962, 6783) -- IDs are for the following Slugs same order ramp-0a5fd2a7, ramp-e8c15113,
+    AND identity_accesses.status IN (6) -- status 6 means it is approved
     -- DEFINE TIMEFRAME WITH ISO
-    AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-05-07' -- GOES BY UTC TIME
-    AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-06-07' -- GOES BY UTC TIME
+    AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-01-01' -- GOES BY UTC TIME
+    AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-07-01' -- GOES BY UTC TIME
 GROUP BY
 	organizations.id,
 	organizations.name,
-	projects.id,
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM'),
+	--projects.id,
 	identity_accesses.status
 ORDER BY
-	"Project_id" ASC;
+	"IA_created_at" ASC;
