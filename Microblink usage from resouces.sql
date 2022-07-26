be4b7a3b1329bbab5788a43fdae64434c05fb10d
muyang <muyang@passbsase.com>
2022-07-26 20:25:09 +0200

    21

--- Microblink usage from resouces.sql
+++ Microblink usage from resouces.sql
@@ -8,9 +8,9 @@ FROM
 	LEFT JOIN identities ON resources.identity_id = identities.id
 	LEFT JOIN identity_accesses ON identities.id = identity_accesses.identity_id
 WHERE
-	TO_CHAR(resources.updated_at, 'YYYY-MM-DD') >= '2021-07-20'
+	TO_CHAR(resources.updated_at, 'YYYY-MM-DD') >= '2021-07-21'
 	AND TO_CHAR(resources.updated_at, 'YYYY-MM-DD') < '2022-07-21'
-	AND identity_accesses.status NOT IN(0, 1) -- this line does not work here, because only finished verification will have resources. -> need to confirm with product
+	AND identity_accesses.status NOT IN(0, 1) -- this line does not work here, though it should have.
 	AND resources.type <> 'SELFIE_VIDEO'
 	AND resources.type <> 'COVID_VACCINATION_CARD'
 	AND resources.type <> 'PROOF_OF_ADDRESS'
