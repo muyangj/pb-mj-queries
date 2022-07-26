e25f347803899e4ea9f469314cd4e19fe350c88c
muyang <muyang@passbsase.com>
2022-07-26 15:25:52 +0200

    added ID rnd report

--- Ramp invoice - number of approved proof of address.sql
+++ Ramp invoice - number of approved proof of address.sql
@@ -13,8 +13,8 @@ FROM
 	LEFT JOIN projects ON identity_accesses.project_id = projects.id
 	LEFT JOIN organizations ON projects.organization_id = organizations.id
 WHERE
-	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-05-07'
-	AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-06-07'
+	TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') >= '2022-06-01'
+	AND TO_CHAR(identity_accesses.created_at, 'YYYY-MM-DD') < '2022-07-01'
 	AND resources.type = 'PROOF_OF_ADDRESS'
 	AND organizations.id = '972' -- ramp org.id 972
 	AND projects.id IN (6783, 1962) -- the projects that we are billing, dunno why we do not bill the other two projects.
