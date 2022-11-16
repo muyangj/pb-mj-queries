30ee94ecf7905052c9c52465edcefe360aeeafa3
muyangpb <muyang@passbase.com>
2022-11-16 17:55:07 +0100

    ss

---
+++ classics/Mintlayer - number of approved verifications.sql
@@ -0,0 +1,29 @@
+SELECT
+	COUNT(DISTINCT identity_accesses.id) as "Number of Approved Verifications",
+	organizations.id AS "Organization_id",
+	organizations.name AS "Organization_name",
+	organizations.stripe_customer_id AS "Stripe_id",
+	--TO_CHAR(identity_accesses.completed_at, 'YYYY-MM') AS "IA_completed_at",
+	--projects.id AS "Project_id",
+	identity_accesses.status as "Status"
+FROM
+    identity_accesses
+    LEFT JOIN aml_profiles ON identity_accesses.identity_id = aml_profiles.identity_id
+    LEFT JOIN projects ON identity_accesses.project_id = projects.id
+    LEFT JOIN organizations on projects.organization_id = organizations.id
+WHERE
+	organizations.stripe_customer_id = 'cus_KQzdfoR14RnCkU'
+    --organizations.id = '972' -- Ramp org_id is 972
+    --AND identity_accesses.project_id IN (1962, 6783) -- IDs are for the following Slugs same order ramp-0a5fd2a7, ramp-e8c15113,
+    -- AND identity_accesses.status IN (6, 7) -- status 6 means it is approved
+    -- DEFINE TIMEFRAME WITH IS
+    AND TO_CHAR(identity_accesses.completed_at, 'YYYY-MM-DD') >= '2022-01-01' -- GOES BY UTC TIME
+    AND TO_CHAR(identity_accesses.completed_at, 'YYYY-MM-DD') < '2022-11-16' -- GOES BY UTC TIME
+GROUP BY
+	organizations.id,
+	organizations.name,
+	--TO_CHAR(identity_accesses.completed_at, 'YYYY-MM'),
+	--projects.id,
+	identity_accesses.status
+--ORDER BY
+	--"IA_created_at" ASC;
--- classics/Number Of Verifications by customer.sql
+++ classics/Number Of Verifications by customer.sql
@@ -3,12 +3,12 @@ SELECT
 	--organizations.id AS "OrganizationID",
 	--organizations.name AS "OrganizationName",
 	organizations.stripe_customer_id AS "Stripe_id",
-	identity_accesses.project_id AS "Project_id",
-	CASE WHEN environment_type = 0 THEN
+	--identity_accesses.project_id AS "Project_id",
+	/*CASE WHEN environment_type = 0 THEN
 		'Sandbox'
 	ELSE
 		'Production'
-	END AS "Plan_type",
+	END AS "Plan_type",*/
 	TO_CHAR(identity_accesses.completed_at, 'YYYY-MM') AS "IA_completed_at"
 FROM
 	identity_accesses
@@ -16,17 +16,17 @@ FROM
 	LEFT JOIN organizations ON projects.organization_id = organizations.id
 WHERE
 	identity_accesses.status NOT IN(0, 1)
-	AND TO_CHAR(identity_accesses.completed_at, 'YYYY-MM-DD') >= '2022-01-01'
+	AND TO_CHAR(identity_accesses.completed_at, 'YYYY-MM-DD') >= '2020-01-01'
 	AND TO_CHAR(identity_accesses.completed_at, 'YYYY-MM-DD') < '2022-11-15'
 	--AND environment_type <> 0
-	AND organizations.stripe_customer_id = 'cus_KPzWRKcKWuhPfl'
+	AND organizations.stripe_customer_id = 'cus_KQzdfoR14RnCkU'
 	--AND organizations.id IN (6988)
 GROUP BY
 	--organizations.id,
 	--organizations.name,
-	environment_type,
+	--environment_type,
 	organizations.stripe_customer_id,
-	identity_accesses.project_id,
+	--identity_accesses.project_id,
 	TO_CHAR(identity_accesses.completed_at, 'YYYY-MM')
 ORDER BY
 	--"NumberOfVerifications" DESC;
